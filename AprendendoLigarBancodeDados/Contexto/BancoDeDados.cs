335ae7d188423aa4374f1cbe6726831de2a80d8e
Alexandra Natacha <alextachaa@gmail.com>
2023-02-10 13:10:18 -0400

    Adicionar arquivos de projeto.

---
+++ AprendendoLigarBancodeDados/Contexto/BancoDeDados.cs
@@ -0,0 +1,14 @@
+﻿using AprendendoLigarBancodeDados.Models.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace AprendendoLigarBancodeDados.Contexto
+{
+    public class BancoDeDados : DbContext
+    {
+        public BancoDeDados(DbContextOptions<BancoDeDados> options) : base(options)
+        {
+        }
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ AprendendoLigarBancodeDados/Migrations/20230210165549_MigrationDOIS.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AprendendoLigarBancodeDados.Migrations
+{
+    public partial class MigrationDOIS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Preco = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
